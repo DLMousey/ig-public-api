1da61b30fc2e3fc6db4529ace0df2bf7dbec2e83
eric <naclericny@gmail.com>
2019-04-05 18:37:16 -0400

    added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+## How to compile
+
+cd into /api and run
+```go build api```
+This will compile and leave the binary in the current directory
+
+```go install```
+Run this if you want to leave the binary in the bin folder of your gopath
+
+## TODO
+* Find out what to cache and how
+* Do the rest of the endpoints
+* Learn interfaces cuz idk how to use em xd
--- common/handler.go
+++ common/handler.go
@@ -1,29 +1,29 @@
 /*
-Snippet taken from:
-https://github.com/mingrammer/go-todo-rest-api-example/blob/master/app/handler/common.go
-
 Will contain common functions for routing and handlerfunctions
 */
 
 package common
 
-/* Not my code xd
-// respondJSON makes the response with payload as json format
-func RespondJSON(w http.ResponseWriter, status int, payload interface{}) {
-	response, err := json.Marshal(payload)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-	w.Write([]byte(response))
-}
+import (
+	"encoding/json"
+	"net/http"
+)
 
-
+/*
 // respondError makes the error response with payload as json format
 func RespondError(w http.ResponseWriter, code int, message string) {
 	RespondJSON(w, code, map[string]string{"error": message})
 }
 */
+
+func RespondJSON(w http.ResponseWriter, status int, data interface{}) {
+	response, err := json.Marshal(data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(response)
+}
